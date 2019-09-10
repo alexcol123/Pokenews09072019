f112f3a7313a3aaeb65954b45a2f8d5353e90d12
Henry Munoz <alexcol123@yahoo.com>
2019-09-09 22:49:18 -0400

    42 MapDispatchToProps in Post component to be able to delet posts

--- src/components/Post.js
+++ src/components/Post.js
@@ -16,12 +16,23 @@ export class Post extends Component {
   //   });
   // }
 
+  handleClick = () => {
+    this.props.deletePost(this.props.post.id);
+    this.props.history.push('/home')
+  };
+
   render() {
-    console.log(this.props.posts);
+    console.log(this.props);
     const post = this.props.post ? (
       <div className="post">
         <h4 className="center">{this.props.post.title}</h4>
         <p>{this.props.post.body}</p>
+
+        <div className="center">
+          <button className="btn grey" onClick={this.handleClick}>
+            Delete Post
+          </button>
+        </div>
       </div>
     ) : (
       <div className="center">Loading Post...</div>
@@ -40,4 +51,14 @@ const mapStateToProps = (state, ownProps) => {
   };
 };
 
-export default connect(mapStateToProps)(Post);
+const mapDispatchToProps = dispatch => {
+  return {
+    deletePost: id => {
+      dispatch({ type: 'DELETE_POST', id: id });
+    }
+  };
+};
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Post);
